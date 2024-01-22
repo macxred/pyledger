857048820762cb0bb671f90edd938778bfd1808b
Lukas <l.elmiger@macxred.ch>
2024-01-22 16:31:04 +0100

    Run tests on GitHub

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+# Read the requirements from the 'requirements.txt' file
+with open('requirements.txt', 'r') as requirements_file:
+    required_packages = requirements_file.readlines()
+
 setup(
     name='pyledger',
     version='v0.0.1',
     python_requires='>3.10',
-    install_requires=['numpy', 'pandas', 'xlsxwriter'],
+    install_requires=required_packages,
     description=('Python package to streamline implementation of or '
                  'connection to accounting systems.'),
     long_description=open('README.md').read(),
