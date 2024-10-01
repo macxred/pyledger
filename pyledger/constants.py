4360ab69d2d3bc674b870b9d556e47e759d2ed9f
Lukas <l.elmiger@macxred.ch>
2024-10-01 21:43:57 +0200

    refactor: Adapt column sequence

--- pyledger/constants.py
+++ pyledger/constants.py
@@ -19,42 +19,42 @@ VAT_CODE_SCHEMA_CSV = """
     text,               string[python],       True
     inclusive,          bool,                 True
     account,            Int64,                True
+    inverse_account,    Int64,                False
     rate,               Float64,              True
     date,               datetime64[ns],       False
-    inverse_account,    Int64,                False
 """
 VAT_CODE_SCHEMA = pd.read_csv(StringIO(VAT_CODE_SCHEMA_CSV), skipinitialspace=True)
 
 ACCOUNT_SCHEMA_CSV = """
     column_name,        dtype,                mandatory
+    group,              string[python],       False
     account,            Int64,                True
     currency,           string[python],       True
-    text,               string[python],       True
     vat_code,           string[python],       False
-    group,              string[python],       False
+    text,               string[python],       True
 """
 ACCOUNT_SCHEMA = pd.read_csv(StringIO(ACCOUNT_SCHEMA_CSV), skipinitialspace=True)
 
 PRICE_SCHEMA_CSV = """
     column_name,        dtype,                mandatory
     ticker,             string[python],       True
+    date,               datetime64[ns],       True
     currency,           string[python],       True
     price,              Float64,              True
-    date,               datetime64[ns],       True
 """
 PRICE_SCHEMA = pd.read_csv(StringIO(PRICE_SCHEMA_CSV), skipinitialspace=True)
 
 LEDGER_SCHEMA_CSV = """
     column_name,         dtype,                mandatory
+    id,                  string[python],       False
     date,                datetime64[ns],       True
     account,             Int64,                True
+    counter_account,     Int64,                False
     currency,            string[python],       True
     amount,              Float64,              True
-    text,                string[python],       True
-    id,                  string[python],       False
-    counter_account,     Int64,                False
     base_currency_amount,Float64,              False
     vat_code,            string[python],       False
+    text,                string[python],       True
     document,            string[python],       False
 """
 LEDGER_SCHEMA = pd.read_csv(StringIO(LEDGER_SCHEMA_CSV), skipinitialspace=True)
