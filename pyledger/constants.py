ad2be8161764bf95bad38ab4a1cfe52cda1b745b
Lukas <l.elmiger@macxred.ch>
2024-10-01 22:52:20 +0200

    reword: Clarify column names

--- pyledger/constants.py
+++ pyledger/constants.py
@@ -9,17 +9,17 @@ FX_ADJUSTMENT_SCHEMA_CSV = """
     adjust,             string[python],       True
     credit,             Int64,                True
     debit,              Int64,                True
-    text,               string[python],       True
+    description,        string[python],       True
 """
 FX_ADJUSTMENT_SCHEMA = pd.read_csv(StringIO(FX_ADJUSTMENT_SCHEMA_CSV), skipinitialspace=True)
 
 VAT_CODE_SCHEMA_CSV = """
     column_name,        dtype,                mandatory
     id,                 string[python],       True
-    text,               string[python],       True
-    inclusive,          bool,                 True
+    description,        string[python],       True
+    is_inclusive,       bool,                 True
     account,            Int64,                True
-    inverse_account,    Int64,                False
+    contra,             Int64,                False
     rate,               Float64,              True
     date,               datetime64[ns],       False
 """
@@ -30,8 +30,8 @@ ACCOUNT_SCHEMA_CSV = """
     group,              string[python],       False
     account,            Int64,                True
     currency,           string[python],       True
-    vat_code,           string[python],       False
-    text,               string[python],       True
+    tax_code,           string[python],       False
+    description,        string[python],       True
 """
 ACCOUNT_SCHEMA = pd.read_csv(StringIO(ACCOUNT_SCHEMA_CSV), skipinitialspace=True)
 
@@ -49,12 +49,12 @@ LEDGER_SCHEMA_CSV = """
     id,                  string[python],       False
     date,                datetime64[ns],       True
     account,             Int64,                True
-    counter_account,     Int64,                False
+    contra,              Int64,                False
     currency,            string[python],       True
     amount,              Float64,              True
-    base_currency_amount,Float64,              False
-    vat_code,            string[python],       False
-    text,                string[python],       True
+    report_amount,       Float64,              False
+    tax_code,            string[python],       False
+    description,         string[python],       True
     document,            string[python],       False
 """
 LEDGER_SCHEMA = pd.read_csv(StringIO(LEDGER_SCHEMA_CSV), skipinitialspace=True)
