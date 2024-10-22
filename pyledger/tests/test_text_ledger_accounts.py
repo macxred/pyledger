300cd82b82d1ccf61b1ad7328614efb9c41df84b
Alex the wizard <sashok1111122222@gmail.com>
2024-10-22 13:19:39 +0300

    feat: add tests to ensure accounts and tax codes mutators does not change the original order of rows

--- pyledger/tests/test_text_ledger_accounts.py
+++ pyledger/tests/test_text_ledger_accounts.py
@@ -3,6 +3,7 @@
 import pytest
 from .base_accounts import BaseTestAccounts
 from pyledger import TextLedger
+from consistent_df import assert_frame_equal
 
 
 class TestAccounts(BaseTestAccounts):
@@ -10,3 +11,30 @@ class TestAccounts(BaseTestAccounts):
     @pytest.fixture
     def ledger(self, tmp_path):
         return TextLedger(tmp_path)
+
+    def test_account_mutators_does_not_change_order(self, ledger):
+        """Test to ensure that mutator functions make minimal invasive changes to accounts file,
+        preserving the original row order so that Git diffs show only the intended modifications.
+        """
+        account_numbers = self.ACCOUNTS["account"].tolist()
+        for account in account_numbers:
+            target = self.ACCOUNTS.query(f"account == {account}").iloc[0].to_dict()
+            ledger.add_account(**target)
+
+        expected = ledger.standardize_accounts(
+            self.ACCOUNTS[self.ACCOUNTS["account"].isin(account_numbers)]
+        )
+        assert_frame_equal(ledger.accounts(), expected, check_like=True)
+
+        expected.loc[expected["account"] == account_numbers[2], "description"] = "New description"
+        target = expected.query(f"account == {account_numbers[2]}").iloc[0].to_dict()
+        ledger.modify_account(**target)
+        expected.loc[expected["account"] == account_numbers[6], "description"] = "New description"
+        target = expected.query(f"account == {account_numbers[6]}").iloc[0].to_dict()
+        ledger.modify_account(**target)
+        assert_frame_equal(ledger.accounts(), expected, check_like=True)
+
+        to_delete = [account_numbers[2], account_numbers[6]]
+        expected = expected.query(f"account not in {to_delete}")
+        ledger.delete_accounts(to_delete)
+        assert_frame_equal(ledger.accounts(), expected, ignore_index=True, check_like=True)
