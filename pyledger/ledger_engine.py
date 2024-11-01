f6bcd4b043069be26fa5432d505ec66b939dbe66
Lukas <l.elmiger@macxred.ch>
2024-11-01 12:16:24 +0100

    refactor: Re-shuffle methods
    Mirroring methods are now direct neighbours.

--- pyledger/ledger_engine.py
+++ pyledger/ledger_engine.py
@@ -1463,25 +1463,6 @@ class LedgerEngine(ABC):
         df = enforce_schema(df, ASSETS_SCHEMA, keep_extra_columns=keep_extra_columns)
         return df
 
-    def prepare_assets_for_mirroring(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Aligns incoming asset data with the current system's constraints.
-
-        Invoked as the initial step in the mirroring process, this method prepares
-        asset data for integration into the current system. It adapts the incoming
-        data to specific storage requirements and aligns it with existing data, making
-        it easy to identify entries that need to be added, modified, or removed.
-
-        By default, this method returns the data unchanged. Subclasses may override
-        it to apply class-specific adaptations as required.
-
-        Args:
-            df (pd.DataFrame): Incoming asset data.
-
-        Returns:
-            pd.DataFrame: Adjusted data ready for synchronization with the current system.
-        """
-        return self.standardize_assets(df)
-
     @abstractmethod
     def assets(self) -> pd.DataFrame:
         """Retrieves all asset entries.
@@ -1597,3 +1578,22 @@ class LedgerEngine(ABC):
             "deleted": len(to_delete) if delete else 0,
             "updated": len(to_update)
         }
+
+    def prepare_assets_for_mirroring(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Aligns incoming asset data with the current system's constraints.
+
+        Invoked as the initial step in the mirroring process, this method prepares
+        asset data for integration into the current system. It adapts the incoming
+        data to specific storage requirements and aligns it with existing data, making
+        it easy to identify entries that need to be added, modified, or removed.
+
+        By default, this method returns the data unchanged. Subclasses may override
+        it to apply class-specific adaptations as required.
+
+        Args:
+            df (pd.DataFrame): Incoming asset data.
+
+        Returns:
+            pd.DataFrame: Adjusted data ready for synchronization with the current system.
+        """
+        return self.standardize_assets(df)
