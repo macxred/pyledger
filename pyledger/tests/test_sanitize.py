c50c22f834fb4b15c31f7caa2285f59b93b07627
Alex the wizard <sashok1111122222@gmail.com>
2024-12-16 21:42:00 +0200

    feat: add test for sanitize_revaluations

--- pyledger/tests/test_sanitize.py
+++ pyledger/tests/test_sanitize.py
@@ -84,3 +84,54 @@ def test_sanitize_prices(engine, capture_logs):
     assert_frame_equal(engine.price_history.standardize(expected_prices), sanitized_prices)
     log_messages = capture_logs.getvalue().strip().split("\n")
     assert len(log_messages) > 0
+
+
+def test_sanitize_revaluations(engine, capture_logs):
+    ACCOUNT_CSV = """
+        group,                       account, currency, tax_code, description
+        Assets,                         1000,      USD,         , Cash in Bank USD
+        Assets,                         1005,      USD,         , Cash in other Bank USD
+        Assets,                         1010,      EUR,         , Cash in Bank EUR
+        Assets,                         1015,      EUR,         , Cash in other Bank EUR
+        Assets,                         1300,      USD,         , VAT Recoverable (Input VAT)
+        Assets,                         1310,      AAA,         , VAT Recoverable (Input VAT)
+        Expenses/Financial Expenses,    7050,      USD,         , Foreign Exchange Gain/Loss
+        Revenue/Financial Gain,         8050,      USD,         , Foreign Exchange Gain
+    """
+    PRICES_CSV = """
+        date,       ticker,  price, currency
+        2023-12-28,    EUR, 1.1068, USD
+    """
+    REVALUATIONS_CSV = """
+        date,         account, debit, credit, description
+                  , 1000:1300,  7050,   8050, Invalid date
+        2023-12-29, 1000:1300,      ,       , No credit nor debit specified
+        2023-12-29, 1000:1300,  7777,       , Not valid debit
+        2023-12-29, 1000:1300,      ,   8888, Not valid credit
+        2023-12-29, 1000:1310,      ,   8050, Invalid currency
+        2020-12-29, 1000:1300,      ,   8050, No price definition
+        2023-12-29, 1000:1300,      ,   8050, Correct revaluation
+        2023-12-29, 1000:1300,  7050,       , Correct revaluation
+        2023-12-29, 1000:1300,  7050,   8050, Correct revaluation
+    """
+    EXPECTED_REVALUATIONS_CSV = """
+        date,         account, debit, credit, description
+        2023-12-29, 1000:1300,      ,   8050, Correct revaluation
+        2023-12-29, 1000:1300,  7050,       , Correct revaluation
+        2023-12-29, 1000:1300,  7050,   8050, Correct revaluation
+    """
+
+    prices = pd.read_csv(StringIO(PRICES_CSV), skipinitialspace=True)
+    accounts = pd.read_csv(StringIO(ACCOUNT_CSV), skipinitialspace=True)
+    revaluations = pd.read_csv(StringIO(REVALUATIONS_CSV), skipinitialspace=True)
+    expected_revaluations = pd.read_csv(StringIO(EXPECTED_REVALUATIONS_CSV), skipinitialspace=True)
+    engine.restore(accounts=accounts, price_history=prices)
+
+    sanitized_revaluations = engine.sanitize_revaluations(
+        engine.revaluations.standardize(revaluations)
+    )
+    assert_frame_equal(
+        engine.revaluations.standardize(expected_revaluations), sanitized_revaluations
+    )
+    log_messages = capture_logs.getvalue().strip().split("\n")
+    assert len(log_messages) > 0
