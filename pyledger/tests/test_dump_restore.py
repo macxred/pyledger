ec7b8e1f05798773897d97deb6accfbbb518ea01
Alex the wizard <sashok1111122222@gmail.com>
2025-04-17 16:17:59 +0300

    feat: introduce new class to test dump clear restore logic for standaloneLedger

--- pyledger/tests/test_dump_restore.py
+++ pyledger/tests/test_dump_restore.py
@@ -1,11 +1,13 @@
 """Test suite for testing dump, restore, and clear operations."""
 
 import pytest
-from .base_test_dump_restore_clear import BaseTestDumpRestoreClear
+from .base_test_standalone_ledger_dump_restore_clear import (
+    BaseTestStandaloneLedgerDumpRestoreClear
+)
 from pyledger import MemoryLedger
 
 
-class TestDumpAndRestore(BaseTestDumpRestoreClear):
+class TestDumpAndRestore(BaseTestStandaloneLedgerDumpRestoreClear):
 
     @pytest.fixture
     def engine(self):
