06a32c5caa80aa8553da12471beedc743a53f0cd
Lukas <l.elmiger@macxred.ch>
2025-04-29 07:15:23 +0200

    fix: Accounting repository parameters

--- profiling.py
+++ profiling.py
@@ -5,8 +5,8 @@ from pyledger import TextLedger
 profiler = Profiler()
 profiler.start()
 
-engine = TextLedger("~/macx/rocket-accounting/accounts")
-engine.account_balance(account=1000, period="2024")
+engine = TextLedger("~/macx/accounts")
+engine.account_balance(account=1020, period="2024")
 
 profiler.stop()
 print(profiler.output_text(unicode=True, color=True))
